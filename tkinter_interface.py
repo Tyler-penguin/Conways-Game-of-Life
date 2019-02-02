6ae878623f21aec8203b54a7ba827da62da6984b
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2019-02-02 01:10:58 -0500

    added an iterate over a period

--- tkinter_interface.py
+++ tkinter_interface.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from defs import *
-
+import time
+import sys
 
 matrix = csv_to_matrix('initial_population.csv')
 def do_something(matrix):
@@ -15,11 +16,55 @@ def do_something(matrix):
             else:
                 w.create_rectangle(new_col*10, new_row*10, new_col*10+10, new_row*10+10, fill="#ffffff")
 
+def continue_doing_something(matrix):
+    frequency = my_entry.get()
+    time_for_iterations = my_entry2.get()
+    total_iterations = int(float(frequency) * float(time_for_iterations))
+    time_between = 1/ float(frequency)
+    for iteration in range(total_iterations):
+
+        do_something(matrix)
+
+        print('OK')
+        time.sleep(time_between)
+        root.update()
+
+    print(frequency, time_for_iterations, total_iterations)
+    my_text.delete(1.0, END)
+    my_text.insert(END, 'DONE')
+
+
 root = Tk()
 root.state('zoomed')
 
-my_button = Button(root, bg='#808080', width=10, command= lambda: do_something(matrix), height=1, text='ITERATE')
-my_button.pack()
+top = Frame(root)
+bottom = Frame(root)
+top.pack(side=TOP)
+bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+my_button = Button(root, bg='#808080', width=20, command= lambda: do_something(matrix), height=1, text='ITERATE ONCE')
+
+my_label = Label(root, text='TIMES PER SECOND')
+my_entry = Entry(root, width=10)
+
+my_label2 = Label(root, text='TIME')
+my_entry2 = Entry(root, width=10)
+
+
+widget = Button(root, bg='#808080', width=20, height=1, command= lambda: continue_doing_something(matrix), text='ITERATE OVER TIME')
+
+my_text = Text(root, width=10, height=1)
+
+my_entry3 = Entry(root, width=10)
+
+my_button.pack(in_=top, side=LEFT)
+my_label.pack(in_=top, side=LEFT)
+my_entry.pack(in_=top, side=LEFT)
+my_label2.pack(in_=top, side=LEFT)
+my_entry2.pack(in_=top, side=LEFT)
+widget.pack(in_=top, side=LEFT)
+my_text.pack(in_=top, side=LEFT)
+
 w = Canvas(root, width=len(matrix[0])*10, height=len(matrix)*10)
 w.pack()
 for new_row in range(len(matrix)):
