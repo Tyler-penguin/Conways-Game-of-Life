55156cfb7b9bef001d03f16ddf20247f46cbda3f
Tyler-penguin <44588551+Tyler-penguin@users.noreply.github.com>
2019-02-03 15:21:04 -0500

    added the option for wrapping boundaries

--- tkinter_interface.py
+++ tkinter_interface.py
@@ -2,9 +2,15 @@ from tkinter import *
 from defs import *
 import time
 
+do_wrap = True
+
+
 matrix = csv_to_matrix('initial_population.csv')
-def do_something(matrix):
-    matrix = iteration_nonwrapping(matrix)
+def do_something(matrix, do_wrap):
+    if do_wrap == True:
+        matrix = iteration_wrapping(matrix)
+    else:
+        matrix = iteration_nonwrapping(matrix)
 #    display_game(matrix, 'O', '.')
 
     w.delete("all")
@@ -16,20 +22,21 @@ def do_something(matrix):
             else:
                 w.create_rectangle(new_col*10, new_row*10, new_col*10+10, new_row*10+10, fill="#ffffff")
 
-def continue_doing_something(matrix):
-    frequency = my_entry.get()
-    time_for_iterations = my_entry2.get()
-    total_iterations = int(float(frequency) * float(time_for_iterations))
-    time_between = 1/ float(frequency)
-    for iteration in range(total_iterations):
+def continue_doing_something(matrix, do_wrap):
+    time_between = float(my_entry.get())
+    number_of_iterations = int(my_entry2.get())
+    iterations_remaining = number_of_iterations
 
-        do_something(matrix)
+    for iteration in range(number_of_iterations):
+
+        do_something(matrix, do_wrap)
+        iterations_remaining -=1
+        my_text.delete(1.0, END)
+        my_text.insert(END, iterations_remaining)
 
         time.sleep(time_between)
-
         root.update()
 
-    print(frequency, time_for_iterations, total_iterations)
     my_text.delete(1.0, END)
     my_text.insert(END, 'DONE')
 
@@ -42,16 +49,16 @@ bottom = Frame(root)
 top.pack(side=TOP)
 bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
 
-my_button = Button(root, bg='#808080', width=20, command= lambda: do_something(matrix), height=1, text='ITERATE ONCE')
+my_button = Button(root, bg='#808080', width=20, command= lambda: do_something(matrix, do_wrap), height=1, text='ITERATE ONCE')
 
-my_label = Label(root, text='TIMES PER SECOND')
+my_label = Label(root, text='TIME BETWEEN UPDATES')
 my_entry = Entry(root, width=10)
 
-my_label2 = Label(root, text='TIME')
+my_label2 = Label(root, text='NUMBER OF ITERATIONS')
 my_entry2 = Entry(root, width=10)
 
 
-widget = Button(root, bg='#808080', width=20, height=1, command= lambda: continue_doing_something(matrix), text='ITERATE OVER TIME')
+widget = Button(root, bg='#808080', width=20, height=1, command= lambda: continue_doing_something(matrix, do_wrap), text='ITERATE OVER TIME')
 
 my_text = Text(root, width=10, height=1)
 
